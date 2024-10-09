e12cdcc7d2c4bdeefbbcd14597970c0b671216d2
truong <TruongLMCE171886@fpt.edu.vn>
2024-10-09 13:34:11 +0700

    commit

--- src/app/recipe-list/[details]/page.tsx
+++ src/app/recipe-list/[details]/page.tsx
@@ -1,16 +1,20 @@
 import RecipeDetailsItem from "@/components/recipe-details";
 
-async function fetchRecipeDetails(currentRecipeId) {
+// Thêm kiểu dữ liệu cho tham số currentRecipeId (ở đây mình chọn kiểu string)
+async function fetchRecipeDetails(currentRecipeId: string) {
     try {
         const apiResponse = await fetch(`https://dummyjson.com/recipes/${currentRecipeId}`);
         const data = await apiResponse.json();
         return data;
     } catch (error) {
-        throw new Error(error)
+        throw new Error(error as string); // Đảm bảo khai báo kiểu dữ liệu cho error
     }
 }
-export default async function RecipeDetails({ params }) {
-    const getRecipeDetails = await fetchRecipeDetails(params?.details)
+
+export default async function RecipeDetails({ params }: { params: { details: string } }) {
+    // Thêm kiểu dữ liệu cho params
+    const getRecipeDetails = await fetchRecipeDetails(params?.details);
+
     return (
         <RecipeDetailsItem getRecipeDetails={getRecipeDetails} />
     );
